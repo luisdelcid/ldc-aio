b7275385230bb390d75405a461ea14df3e85d421
Luis del Cid <luisdelcid@Luiss-MacBook.local>
2020-03-24 17:51:03 -0600

    LDC

---
+++ all/beaver-builder-theme/beaver-builder-theme.php
@@ -0,0 +1,42 @@
+<?php
+
+class LDC_AIO_Beaver_Builder_Theme {
+
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    static public function after_setup_theme(){
+        $meta_box_and_tab = 'Beaver Builder Theme';
+        LDC_AIO_One::add_setting('remove_default_styles', array(
+        	'name' => 'Remove default styles for HTML buttons and forms?',
+        	'on_label' => '<i class="dashicons dashicons-yes"></i>',
+        	'style' => 'square',
+        	'type' => 'switch',
+        ), $meta_box_and_tab);
+        $remove_default_styles = LDC_AIO_One::get_setting('remove_default_styles');
+        if($remove_default_styles){
+            add_filter('fl_theme_compile_less_paths', array(__CLASS__, 'fl_theme_compile_less_paths'));
+        }
+    }
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    static public function init(){
+        add_action('after_setup_theme', array(__CLASS__, 'after_setup_theme'));
+	}
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    static public function fl_theme_compile_less_paths($paths){
+        foreach($paths as $index => $path){
+            if($path == FL_THEME_DIR . '/less/theme.less'){
+                $paths[$index] = FL_CHILD_THEME_DIR . '/less/theme.less';
+            }
+        }
+        return $paths;
+	}
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+}
+
+LDC_AIO_Admin_Search_Meta::init();
