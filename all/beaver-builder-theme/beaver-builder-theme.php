59f5a5fb594ffacabf1db1e1a2990f95907eb656
Luis del Cid <luisdelcid@Luiss-MacBook.local>
2020-03-24 18:37:27 -0600

    LDC

--- all/beaver-builder-theme/beaver-builder-theme.php
+++ all/beaver-builder-theme/beaver-builder-theme.php
@@ -7,21 +7,22 @@ class LDC_AIO_Beaver_Builder_Theme {
     static public function after_setup_theme(){
         $meta_box_and_tab = 'Beaver Builder Theme';
         LDC_AIO_One::add_setting('remove_default_styles', array(
+            'desc' => 'You must update the <a href="' . esc_url(admin_url('customize.php')) . '">Customizer</a> for new settings to take effect.',
         	'name' => 'Remove default styles for HTML buttons and forms?',
         	'on_label' => '<i class="dashicons dashicons-yes"></i>',
         	'style' => 'square',
         	'type' => 'switch',
         ), $meta_box_and_tab);
-        $remove_default_styles = LDC_AIO_One::get_setting('remove_default_styles');
-        if($remove_default_styles){
-            add_filter('fl_theme_compile_less_paths', array(__CLASS__, 'fl_theme_compile_less_paths'));
-        }
     }
 
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     static public function init(){
         add_action('after_setup_theme', array(__CLASS__, 'after_setup_theme'));
+		$remove_default_styles = LDC_AIO_One::get_setting('remove_default_styles');
+        if($remove_default_styles){
+            add_filter('fl_theme_compile_less_paths', array(__CLASS__, 'fl_theme_compile_less_paths'));
+        }
 	}
 
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
