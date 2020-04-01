8c1c6610825eeee468c400944956e40d8dbe623e
Luis del Cid <luisdelcid@Luiss-MacBook.local>
2020-04-01 14:32:44 -0600

    LDC

--- all/zoom/functions.php
+++ all/zoom/functions.php
@@ -3,14 +3,14 @@
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 if(!function_exists('ldc_generate_jwt')){
-	function ldc_generate_jwt($api_key = '', $api_secret = ''){
-		if($api_key and $api_secret){
+	function ldc_generate_jwt($iss = '', $key = ''){
+		if($iss and $key){
 			require_once(LDC_AIO_DIR . 'in/php-jwt-5.2.0/vendor/autoload.php');
-			$token = array(
+			$payload = array(
+				'iss' => $iss,
 				'exp' => time() + MINUTE_IN_SECONDS, // GMT time
-				'iss' => $api_key,
 			);
-			return \Firebase\JWT\JWT::encode($token, $api_secret);
+			return \Firebase\JWT\JWT::encode($payload, $key);
 		}
 		return '';
 	}
@@ -22,18 +22,83 @@ if(!function_exists('ldc_zoom_request')){
 	function ldc_zoom_request($api = '', $point = '', $arguments = array()){
 		if($api and $point){
 			$endpoints = array(
-	            'dashboards' => array(
-	                'list_meetings' => array(
-	                    'endpoint' => '/metrics/meetings',
+	            'cloud_recording' => array(
+	                'list_all_recordings' => array(
+	                    'endpoint' => '/users/%s/recordings',
 	                    'method' => 'GET',
-	                    'parameters' => 0,
+	                    'parameters' => 1,
+	                ),
+					'get_meeting_recordings' => array(
+	                    'endpoint' => '/meetings/%s/recordings',
+	                    'method' => 'GET',
+	                    'parameters' => 1,
+	                ),
+					'delete_meeting_recordings' => array(
+	                    'endpoint' => '/meetings/%s/recordings',
+	                    'method' => 'DELETE',
+	                    'parameters' => 1,
+	                ),
+					'delete_a_meeting_recording_file' => array(
+	                    'endpoint' => '/meetings/%s/recordings/%s',
+	                    'method' => 'DELETE',
+	                    'parameters' => 2,
+	                ),
+					'recover_meeting_recordings' => array(
+	                    'endpoint' => '/meetings/%s/recordings/status',
+	                    'method' => 'PUT',
+	                    'parameters' => 1,
+	                ),
+					'recover_a_single_recording' => array(
+	                    'endpoint' => '/meetings/%s/recordings/%s/status',
+	                    'method' => 'PUT',
+	                    'parameters' => 2,
+	                ),
+					'get_meeting_recording_settings' => array(
+	                    'endpoint' => '/meetings/%s/recordings/settings',
+	                    'method' => 'GET',
+	                    'parameters' => 1,
+	                ),
+					'update_meeting_recording_settings' => array(
+	                    'endpoint' => '/meetings/%s/recordings/settings',
+	                    'method' => 'PATCH',
+	                    'parameters' => 1,
+	                ),
+					'list_recording_registrants' => array(
+	                    'endpoint' => '/meetings/%d/recordings/registrants',
+	                    'method' => 'GET',
+	                    'parameters' => 1,
+	                ),
+					'create_a_recording_registrant' => array(
+	                    'endpoint' => '/meetings/%d/recordings/registrants',
+	                    'method' => 'POST',
+	                    'parameters' => 1,
+	                ),
+					'update_recording_registrants_status' => array(
+	                    'endpoint' => '/meetings/%d/recordings/registrants/status',
+	                    'method' => 'PUT',
+	                    'parameters' => 1,
+	                ),
+					'get_registration_questions' => array(
+	                    'endpoint' => '/meetings/%s/recordings/registrants/questions',
+	                    'method' => 'GET',
+	                    'parameters' => 1,
+	                ),
+					'update_registration_questions' => array(
+	                    'endpoint' => '/meetings/%s/recordings/registrants/questions',
+	                    'method' => 'PATCH',
+	                    'parameters' => 1,
+	                ),
+					'list_recordings_of_an_account' => array(
+	                    'endpoint' => '/accounts/%s/recordings',
+	                    'method' => 'GET',
+	                    'parameters' => 1,
 	                ),
 	            ),
 	        );
 			$endpoints = apply_filters('ldc_zoom_endpoints', $endpoints);
 	        if(!empty($endpoints[$api][$point])){
 				$endpoint = $endpoints[$api][$point]['endpoint'];
-		        $method = $endpoints[$api][$point]['method'];
+		        $method = strtoupper($endpoints[$api][$point]['method']);
 		        $parameters = $endpoints[$api][$point]['parameters'];
 		        if(count($arguments) >= $parameters){
 					if($parameters){
