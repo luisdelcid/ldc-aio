9d9dafae5549857cd006344796d9854b305f3f8a
Luis del Cid <luisdelcid@Luiss-MacBook.local>
2020-04-01 20:31:36 -0600

    LDC

---
+++ all/google/functions.php
@@ -0,0 +1,26 @@
+<?php
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if(!function_exists('ldc_google_client')){
+	function ldc_google_client($config = array()){
+        if(class_exists('Google_Client', false)){
+            return new Google_Client($config);
+        }
+		return null;
+	}
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if(!function_exists('ldc_google_application_credentials')){
+	function ldc_google_application_credentials($file = ''){
+		if(file_exists($file)){
+			putenv('GOOGLE_APPLICATION_CREDENTIALS=' . $file);
+			return true;
+		}
+		return false;
+	}
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
