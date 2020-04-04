49fe51e9767fa6937cc8651442a753a1e4852b51
Luis del Cid <luisdelcid@Luiss-MacBook.local>
2020-04-04 11:35:48 -0600

    LDC

--- all/media/media.php
+++ all/media/media.php
@@ -6,16 +6,6 @@ class LDC_AIO_Media {
 
     static public function init(){
         $meta_box_and_tab = 'Media';
-        LDC_AIO_One::add_setting('fix_video_mp4_mime_type_conflicts', array(
-        	'name' => 'Fix video/mp4 mime type conflicts?',
-        	'on_label' => '<i class="dashicons dashicons-yes"></i>',
-        	'style' => 'square',
-        	'type' => 'switch',
-        ), $meta_box_and_tab);
-        $fix_video_mp4_mime_type_conflicts = LDC_AIO_One::get_setting('fix_video_mp4_mime_type_conflicts');
-        if($fix_video_mp4_mime_type_conflicts){
-            add_filter('wp_check_filetype_and_ext', array(__CLASS__, 'wp_check_filetype_and_ext'), 10, 5);
-        }
         LDC_AIO_One::add_setting('remove_filename_accents', array(
         	'name' => 'Remove filename accents?',
         	'on_label' => '<i class="dashicons dashicons-yes"></i>',
@@ -25,6 +15,16 @@ class LDC_AIO_Media {
         $remove_filename_accents = LDC_AIO_One::get_setting('remove_filename_accents');
         if($remove_filename_accents){
             add_filter('sanitize_file_name', array(__CLASS__, 'sanitize_file_name'));
+        }
+        LDC_AIO_One::add_setting('solve_audio_video_conflicts', array(
+        	'name' => 'Solve audio/video conflicts?',
+        	'on_label' => '<i class="dashicons dashicons-yes"></i>',
+        	'style' => 'square',
+        	'type' => 'switch',
+        ), $meta_box_and_tab);
+        $solve_audio_video_conflicts = LDC_AIO_One::get_setting('solve_audio_video_conflicts');
+        if($solve_audio_video_conflicts){
+            add_filter('wp_check_filetype_and_ext', array(__CLASS__, 'wp_check_filetype_and_ext'), 10, 5);
         }
 	}
 
@@ -40,13 +40,13 @@ class LDC_AIO_Media {
         if($wp_check_filetype_and_ext['ext'] and $wp_check_filetype_and_ext['type']){
     		return $wp_check_filetype_and_ext;
     	}
-    	if($real_mime == 'video/mp4'){
-    		$filetype = wp_check_filetype($filename);
-    		if(in_array($filetype['ext'], wp_get_audio_extensions()) or in_array($filetype['ext'], wp_get_video_extensions())){
-    			$wp_check_filetype_and_ext['ext'] = $filetype['ext'];
+        if(0 === strpos($real_mime, 'audio/') or 0 === strpos($real_mime, 'video/')){
+            $filetype = wp_check_filetype($filename);
+            if(in_array(substr($filetype['type'], 0, strcspn($filetype['type'], '/')), array('audio', 'video'))){
+                $wp_check_filetype_and_ext['ext'] = $filetype['ext'];
     			$wp_check_filetype_and_ext['type'] = $filetype['type'];
-    		}
-    	}
+            }
+        }
     	return $wp_check_filetype_and_ext;
     }
 
