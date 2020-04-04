70ca86eb207f17d392646456fe9a675d443c9f79
Luis del Cid <luisdelcid@Luiss-MacBook.local>
2020-04-04 15:56:45 -0600

    LDC

--- all/media/media.php
+++ all/media/media.php
@@ -4,8 +4,28 @@ class LDC_AIO_Media {
 
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+    static public function image_size_names_choose($sizes){
+        $sizes['hd'] = 'HD';
+        $sizes['full-hd'] = 'Full HD';
+		return $sizes;
+    }
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     static public function init(){
         $meta_box_and_tab = 'Media';
+        LDC_AIO_One::add_setting('add_hd_and_full_hd_image_size', array(
+        	'name' => 'Add HD and Full HD image sizes?',
+        	'on_label' => '<i class="dashicons dashicons-yes"></i>',
+        	'style' => 'square',
+        	'type' => 'switch',
+        ), $meta_box_and_tab);
+        $add_hd_and_full_hd_image_size = LDC_AIO_One::get_setting('add_hd_and_full_hd_image_size');
+        if($add_hd_and_full_hd_image_size){
+            add_image_size('hd', 1280, 1280);
+            add_image_size('full-hd', 1920, 1920);
+            add_filter('image_size_names_choose', array(__CLASS__, 'image_size_names_choose'));
+        }
         LDC_AIO_One::add_setting('remove_filename_accents', array(
         	'name' => 'Remove filename accents?',
         	'on_label' => '<i class="dashicons dashicons-yes"></i>',
